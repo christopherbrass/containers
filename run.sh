0923074e33913634e04cd538888fd17ba3f2cc15
Christopher Brass <christopher.brass871@gmail.com>
2025-02-15 18:04:13 -0600

    my_workspace init

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo docker pull ghcr.io/christopherbrass/containers:my_workspace
+sudo docker image prune -f
 sudo docker run --interactive --tty --rm --mount type=bind,source="$(pwd)"/,target=/workspace --workdir=/workspace --name my_workspace_temp ghcr.io/christopherbrass/containers:my_workspace fish
